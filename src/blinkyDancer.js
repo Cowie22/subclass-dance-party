7aee83c2ce603257162dacb6e0c9d52cc2a6cc4c
Cowie22 <rjcowie@ucla.edu>
2019-01-09 19:13:31 -0800

    "Progress made. Currently working on blinky dancer and dancer.step"

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,6 +1,6 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-  makeDancer.call(this);
+  Dancer.call(this, top, left, timeBetweenSteps);
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
 
@@ -8,7 +8,7 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
 };
 
-makeBlinkyDancer.prototype = new makeDancer();
+makeBlinkyDancer.prototype = new Dancer(top, left, timeBetweenSteps);
 makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
