c21928687f2f2cc3555b1cabd448295289498907
Cowie22 <rjcowie@ucla.edu>
2019-01-09 21:25:06 -0800

    "begining to get tests to pass"

--- src/dancer.js
+++ src/dancer.js
@@ -1,5 +1,5 @@
 // Creates and returns a new dancer object that can step
-var Dancer = function(top, left, timeBetweenSteps) {
+var MakeDancer = function(top, left, timeBetweenSteps) {
 
   //var dancer = {};
   this.top = top;
@@ -21,7 +21,7 @@ var Dancer = function(top, left, timeBetweenSteps) {
   //debugger;
 };
 
-Dancer.prototype.step = function() {
+MakeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
   setTimeout(this.step.bind(this), this.timeBetweenSteps);
@@ -29,12 +29,11 @@ Dancer.prototype.step = function() {
 };
 
 
-Dancer.prototype.setPosition = function(top, left) {
+MakeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
 // where it belongs on the page. See http://api.jquery.com/css/
 //
   var styleSettings = {
-    //do we add the position property? 
     top: top, 
     left: left
   };
