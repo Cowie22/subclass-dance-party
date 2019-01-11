99947b9934b73ce36d2fef76cde10c9ef58cf451
Cowie22 <rjcowie@ucla.edu>
2019-01-10 20:31:09 -0800

    added style to partner up button

--- src/init.js
+++ src/init.js
@@ -23,23 +23,22 @@ $(document).ready(function() {
     // make a dancer with a random position
 
     var dancer = new dancerMakerFunction(
-      $("body").height() * Math.random(),
-      $("body").width() * Math.random(),
+      $('body').height() * Math.random(),
+      $('body').width() * Math.random(),
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
     window.dancers.push(dancer);
     
     $('.line-up').on('click', function() {
-      $('.meeseeks').css({left: 50, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
-      $('.birdPerson').css({left: 1800, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
-      $('.blinky').css({top: 50, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
-      $('.MrPBH').css({top: 820, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
+      $('.meeseeks').css({left: 50, 'justify-content': 'space-between', 'display': 'flex', 'flex-wrap': 'wrap'});
+      $('.birdPerson').css({left: 1800, 'justify-content': 'space-between', 'display': 'flex', 'flex-wrap': 'wrap'});
+      $('.blinky').css({top: 50, 'justify-content': 'space-between', 'display': 'flex', 'flex-wrap': 'wrap'});
+      $('.MrPBH').css({top: 820, 'justify-content': 'space-between', 'display': 'flex', 'flex-wrap': 'wrap'});
     });
     
     $('.birdPerson').on('mouseover', function() {
-      //var upsideDown = false;
-      if ($(this).hasClass('upsidedown') === false) {//!upsideDown) {
+      if ($(this).hasClass('upsidedown') === false) {
         $(this).css({'transform': 'rotate(180deg)', 'transition-duration': '1s'});
         $(this).addClass('upsidedown'); 
         
@@ -47,25 +46,14 @@ $(document).ready(function() {
         $(this).css({'transform': 'rotate(360deg)', 'transition-duration': '1s'});
         $(this).removeClass('upsidedown');
       }
-      //$(this).css({'transform': 'rotate(180deg)', 'transition-duration': '0.5s'});
-     // $(this).css({'transform': 'rotate(360deg)', 'transition-duration': '0.5s'});
     });
     
-    //were going to need a button to perform nearest dancer functionality
     $('.make-dance-together').on('click', function() {
-      //when we click the button
-      //we want the dancers to dance together
-      //we will loop the window.dancer array
-        //this array is made up of object
-        //in order to calculate distance
-        //we'll have var positionTop = (array[i].top)^2
-        //we'll have var positionLeft = (array[i].left)^2
-        //var distance = Math.sqrt(positionTop + positionLeft)
-        var shortestDistance;
-        var objDancer1 = {};
-        var objDancer2 = {};
-        if (window.dancers.length > 1) {
-          for (let i = 0; i < window.dancers.length; i++) {
+      var shortestDistance;
+      var objDancer1 = {};
+      var objDancer2 = {};
+      if (window.dancers.length > 1) {
+        for (let i = 0; i < window.dancers.length; i++) {
           for (let j = i + 1; j < window.dancers.length; j++) {
             //j might be 0
             let firstPosition = window.dancers[i];
@@ -87,8 +75,8 @@ $(document).ready(function() {
       }
       objDancer1.$node.addClass('dancer1');
       objDancer2.$node.addClass('dancer2');
-      $('.dancer1').css({left: 900, top: 450});
-      $('.dancer2').css({left: 900, top: 450});
+      $('.dancer1').css({left: 955, top: 350, 'max-height': 300, 'max-width': 300, 'transform': 'rotateY(180deg)', 'transition': 'transform 4s'});
+      $('.dancer2').css({left: 855, top: 350, 'max-height': 300, 'max-width': 300, 'transform': 'rotateX(180deg)', 'transition-duration': '4s'});
         
     });
     
