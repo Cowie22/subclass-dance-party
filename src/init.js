c37d559026cfffcb679aad01b00217d17082b192
Cowie22 <rjcowie@ucla.edu>
2019-01-10 20:01:50 -0800

    functionality added for partner-up

--- src/init.js
+++ src/init.js
@@ -28,12 +28,13 @@ $(document).ready(function() {
       Math.random() * 1000
     );
     $('body').append(dancer.$node);
+    window.dancers.push(dancer);
     
     $('.line-up').on('click', function() {
-      $('.meeseeks').css({left: 200, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
-      $('.birdPerson').css({left: 1200, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
-      $('.blinky').css({top: 100, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
-      $('.MrPBH').css({top: 600, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
+      $('.meeseeks').css({left: 50, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
+      $('.birdPerson').css({left: 1800, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
+      $('.blinky').css({top: 50, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
+      $('.MrPBH').css({top: 820, "justify-content": "space-between", "display": "flex", "flex-wrap": "wrap"});
     });
     
     $('.birdPerson').on('mouseover', function() {
@@ -43,13 +44,54 @@ $(document).ready(function() {
         $(this).addClass('upsidedown'); 
         
       } else if ($(this).hasClass('upsidedown') === true) {
-        console.log('hello world')
         $(this).css({'transform': 'rotate(360deg)', 'transition-duration': '1s'});
         $(this).removeClass('upsidedown');
       }
       //$(this).css({'transform': 'rotate(180deg)', 'transition-duration': '0.5s'});
      // $(this).css({'transform': 'rotate(360deg)', 'transition-duration': '0.5s'});
     });
+    
+    //were going to need a button to perform nearest dancer functionality
+    $('.make-dance-together').on('click', function() {
+      //when we click the button
+      //we want the dancers to dance together
+      //we will loop the window.dancer array
+        //this array is made up of object
+        //in order to calculate distance
+        //we'll have var positionTop = (array[i].top)^2
+        //we'll have var positionLeft = (array[i].left)^2
+        //var distance = Math.sqrt(positionTop + positionLeft)
+        var shortestDistance;
+        var objDancer1 = {};
+        var objDancer2 = {};
+        if (window.dancers.length > 1) {
+          for (let i = 0; i < window.dancers.length; i++) {
+          for (let j = i + 1; j < window.dancers.length; j++) {
+            //j might be 0
+            let firstPosition = window.dancers[i];
+            let secondPosition = window.dancers[j];
+            let distance = Math.sqrt(Math.pow((firstPosition.top - secondPosition.top), 2) + Math.pow((firstPosition.left - secondPosition.left), 2));
+            if (shortestDistance === undefined) {
+              objDancer1 = firstPosition;
+              objDancer2 = secondPosition;
+              shortestDistance = distance;
+            } else {
+              if (shortestDistance > distance) {
+                shortestDistance = distance;
+                objDancer1 = firstPosition;
+                objDancer2 = secondPosition;
+              }
+            }
+          }
+        }
+      }
+      objDancer1.$node.addClass('dancer1');
+      objDancer2.$node.addClass('dancer2');
+      $('.dancer1').css({left: 900, top: 450});
+      $('.dancer2').css({left: 900, top: 450});
+        
+    });
+    
   });
 });
 
